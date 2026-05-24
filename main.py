1e6b0ae5e68b9cba0ae6f533ed7ad36cf2670dfd
Tom Van Gael <r1114549@student.thomasmore.be>
2026-05-24 19:44:11 +0200

    Update main.py with router connection student1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import config
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware import Middleware
 from routes import student4_endpoints
+from routes import student1_endpoints
 
 middleware = [
     Middleware(
@@ -18,6 +19,7 @@ middleware = [
 app = FastAPI(docs_url=config.documentation_url,middleware=middleware)
 
 app.include_router(student4_endpoints.router)
+app.include_router(student1_endpoints.router)
 
 @app.get("/")
 def root():
