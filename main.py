8b637d5320554be69f951cdc4fd21505ad8d9a88
=Eddy Mouws <=>
2026-05-28 20:10:23 +0200

    Commented out Rene's import to prevent errors since file was empty

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 import config
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware import Middleware
-from routes import student1_endpoints, student2_endpoints, student3_endpoints, student4_endpoints
+#from routes import student1_endpoints, student2_endpoints, student3_endpoints, student4_endpoints
+from routes import student1_endpoints, student3_endpoints, student4_endpoints
 
 middleware = [
     Middleware(
@@ -19,7 +20,7 @@ middleware = [
 app = FastAPI(docs_url=config.documentation_url,middleware=middleware)
 
 app.include_router(student1_endpoints.router)
-app.include_router(student2_endpoints.router)
+#app.include_router(student2_endpoints.router)
 app.include_router(student3_endpoints.router)
 app.include_router(student4_endpoints.router)
 
