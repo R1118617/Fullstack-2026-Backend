ab3e9de271b35022d882637291b5ab98224b18c9
René Junior Van Asch <rene.jr.van.asch@gmail.com>
2026-05-29 19:07:49 +0200

    nog eens fix style

--- queries/student2_queries.py
+++ queries/student2_queries.py
@@ -1,15 +1,18 @@
 query_insert_message = """
-                           INSERT INTO fitness.contact_messages (name, reason, email, message, terms_accepted)
+                           INSERT INTO fitness.contact_messages
+                           (name, reason, email, message, terms_accepted)
                            VALUES (%s, %s, %s, %s, %s);
                        """
 
 query_get_all_messages = """
-                             SELECT id, name, reason, email, message, terms_accepted, created_at
+                             SELECT id, name, reason, email,
+                             message, terms_accepted, created_at
                              FROM fitness.contact_messages;
                          """
 
 query_get_messages_by_reason = """
-                                   SELECT id, name, reason, email, message, terms_accepted, created_at
+                                   SELECT id, name, reason, email,
+                                   message, terms_accepted, created_at
                                    FROM fitness.contact_messages
                                    WHERE reason = %s;
                                """
