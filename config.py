b85473cfe86e677bf1bec6319e41493254a7035f
=Eddy Mouws <=>
2026-05-29 06:17:50 +0200

    try to fix dbconnect string#3

--- config.py
+++ config.py
@@ -2,7 +2,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-db_connection = os.environ.get('DB_CONNECT')
+#db_connection = os.environ.get('DB_CONNECT')
 documentation_url = os.environ.get('DOCS_URL')
 #allowed_origins = os.environ.get('ALLOWED_ORIGINS', "http://localhost:63342,http://127.0.0.1:63342,http://localhost:8000")
 allowed_origins = os.environ.get('ALLOWED_ORIGINS')
+db_user = os.environ.get('DB_CONNECT_USER')
+db_password = os.environ.get('DB_CONNECT_PASSWORD')
+db_url = os.environ.get('DB_CONNECT_URL')
+db_dbname = os.environ.get('DB_CONNECT_DATABASE')
+db_connection = "postgresql://".db_user + ":" + db_password + "@" + db_url + "/"+ db_dbname + "?sslmode=require&channel_binding=require"
